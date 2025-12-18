f6002db9ed60f2e87f682cdef3cc52042a388eed
frankyuhas <55010775+frankyuhas@users.noreply.github.com>
2025-12-18 14:44:39 -0500

    Update image upload server configuration and comments

--- image_upload_server.py
+++ image_upload_server.py
@@ -14,7 +14,7 @@ import os
 # USER SETTINGS
 # ==========================
 
-IMAGE_FOLDER = "/home/pictureframe/images"
+IMAGE_FOLDER = os.path.join(BASE_DIR, "static/images")
 ALLOWED_EXTENSIONS = {".png", ".jpg", ".jpeg", ".bmp"}
 PORT = 8080   # Web page will be http://pi-ip:8080
 
@@ -91,6 +91,7 @@ def upload():
 if __name__ == "__main__":
     # Bind to all interfaces so LAN devices can access it
     app.run(host="0.0.0.0", port=PORT)
+
 
 
 
